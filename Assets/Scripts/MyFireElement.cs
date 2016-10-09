1307b6494bc1b53c76acae26165291b15a97a66d
SidneyFanFan <SidneyFan716@gmail.com>
2016-10-08 22:55:16 -0700

    joysticker is able to move elements

--- Assets/Scripts/MyFireElement.cs
+++ Assets/Scripts/MyFireElement.cs
@@ -12,6 +12,7 @@ public class MyFireElement: Photon.MonoBehaviour
 	private Rigidbody2D rb2d;
 
 	#region MONOBEHAVIOUR MESSAGES
+
 	void Start ()
 	{
 		rb2d = GetComponent<Rigidbody2D> ();
@@ -19,16 +20,27 @@ public class MyFireElement: Photon.MonoBehaviour
 
 	void Update ()
 	{
-		if( photonView.isMine == false && PhotonNetwork.connected == true )
-		{
+		if (photonView.isMine == false && PhotonNetwork.connected == true) {
 			return;
 		}
-		Vector2 moveVec = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical")) * speed; 
-		rb2d.position += moveVec.normalized * speed * scale;
+	
 	}
+
+	void FixedUpdate ()
+	{
+		if (photonView.isMine == false && PhotonNetwork.connected == true) {
+			return;
+		}
+
+		Vector2 moveVec = new Vector2 (CrossPlatformInputManager.GetAxis ("LeftHorizontal"), CrossPlatformInputManager.GetAxis ("LeftVertical")) * speed; 
+		rb2d.position += moveVec.normalized * speed * scale;
+		Vector2 shootVec = new Vector2 (CnInputManager.GetAxis ("Horizontal"), CnInputManager.GetAxis ("Vertical"));
+		if (shootVec.magnitude > 0) {
+			float angle = Mathf.Atan2 (shootVec.y, shootVec.x) * Mathf.Rad2Deg - 90f;
+			rb2d.rotation = angle;
+		}
+
+	}
+
 	#endregion
-				
-
-
-
 }
