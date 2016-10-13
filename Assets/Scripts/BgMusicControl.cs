8ac6b96ac51b567b0ad449270a6a1376e3765888
Rui Gan <ruigan@usc.edu>
2016-10-13 14:19:35 -0700

    add bg music

---
+++ Assets/Scripts/BgMusicControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BgMusicControl : MonoBehaviour {
+
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+		audioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void switchMusic() {
+		if (audioSource.isPlaying) {
+			audioSource.Stop ();
+		} else {
+			audioSource.Play ();
+		}
+	}
+}
