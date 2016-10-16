d7ad7342e278956d78cc75cb179e10fb498b4213
SidneyFanFan <SidneyFan716@gmail.com>
2016-10-16 00:50:52 -0700

    Add background; Adjust camera

--- Assets/Scripts/CameraWork.cs
+++ Assets/Scripts/CameraWork.cs
@@ -14,23 +14,23 @@ namespace Com.EW.MyGame
 		#region Public Properties
 
 
-		[Tooltip("The distance in the local x-z plane to the target")]
+		[Tooltip ("The distance in the local x-z plane to the target")]
 		public float distance = 7.0f;
 
 
-		[Tooltip("The height we want the camera to be above the target")]
+		[Tooltip ("The height we want the camera to be above the target")]
 		public float height = 3.0f;
 
 
-		[Tooltip("The Smooth time lag for the height of the camera.")]
-		public float heightSmoothLag = 0.3f;
+		[Tooltip ("The Smooth time lag for the height of the camera.")]
+		public float heightSmoothLag = 0.1f;
 
 
-		[Tooltip("Allow the camera to be offseted vertically from the target, for example giving more view of the sceneray and less ground.")]
+		[Tooltip ("Allow the camera to be offseted vertically from the target, for example giving more view of the sceneray and less ground.")]
 		public Vector3 centerOffset = Vector3.zero;
 
 
-		[Tooltip("Set this as false if a component of a prefab being instanciated by Photon Network, and manually call OnStartFollowing() when and if needed.")]
+		[Tooltip ("Set this as false if a component of a prefab being instanciated by Photon Network, and manually call OnStartFollowing() when and if needed.")]
 		public bool followOnStart = false;
 
 
@@ -65,12 +65,11 @@ namespace Com.EW.MyGame
 		/// <summary>
 		/// MonoBehaviour method called on GameObject by Unity during initialization phase
 		/// </summary>
-		void Start()
+		void Start ()
 		{
 			// Start following the target if wanted.
-			if (followOnStart)
-			{
-				OnStartFollowing();
+			if (followOnStart) {
+				OnStartFollowing ();
 			}
 
 
@@ -80,13 +79,12 @@ namespace Com.EW.MyGame
 		/// <summary>
 		/// MonoBehaviour method called after all Update functions have been called. This is useful to order script execution. For example a follow camera should always be implemented in LateUpdate because it tracks objects that might have moved inside Update.
 		/// </summary>
-		void LateUpdate()
+		void LateUpdate ()
 		{
 			// The transform target may not destroy on level load, 
 			// so we need to cover corner cases where the Main Camera is different everytime we load a new scene, and reconnect when that happens
-			if (cameraTransform == null && isFollowing)
-			{
-				OnStartFollowing();
+			if (cameraTransform == null && isFollowing) {
+				OnStartFollowing ();
 			}
 
 
@@ -107,12 +105,11 @@ namespace Com.EW.MyGame
 		/// Raises the start following event. 
 		/// Use this when you don't know at the time of editing what to follow, typically instances managed by the photon network.
 		/// </summary>
-		public void OnStartFollowing()
+		public void OnStartFollowing ()
 		{         
 			cameraTransform = Camera.main.transform;
 			isFollowing = true;
-			// we don't smooth anything, we go straight to the right camera shot
-			Cut();
+			Apply ();
 		}
 
 
@@ -125,86 +122,12 @@ namespace Com.EW.MyGame
 		/// <summary>
 		/// Follow the target smoothly
 		/// </summary>
-		void Apply()
+		void Apply ()
 		{
 			Vector3 targetCenter = transform.position + centerOffset;
-
-
-			// Calculate the current & target rotation angles
-			float originalTargetAngle = transform.eulerAngles.y;
-			float currentAngle = cameraTransform.eulerAngles.y;
-
-
-			// Adjust real target angle when camera is locked
-			float targetAngle = originalTargetAngle;
-
-
-			currentAngle = targetAngle;
-
-
-			targetHeight = targetCenter.y + height;
-
-
-			// Damp the height
-			float currentHeight = cameraTransform.position.y;
-			currentHeight = Mathf.SmoothDamp( currentHeight, targetHeight, ref heightVelocity, heightSmoothLag );
-
-
-			// Convert the angle into a rotation, by which we then reposition the camera
-			Quaternion currentRotation = Quaternion.Euler( 0, currentAngle, 0 );
-
-
-			// Set the position of the camera on the x-z plane to:
-			// distance meters behind the target
-			cameraTransform.position = targetCenter;
-			cameraTransform.position += currentRotation * Vector3.back * distance;
-
-
-			// Set the height of the camera
-			cameraTransform.position = new Vector3( cameraTransform.position.x, currentHeight, cameraTransform.position.z );
-
-
-			// Always look at the target    
-			SetUpRotation(targetCenter);
-		}
-
-
-		/// <summary>
-		/// Directly position the camera to a the specified Target and center.
-		/// </summary>
-		void Cut( )
-		{
-			float oldHeightSmooth = heightSmoothLag;
-			heightSmoothLag = 0.001f;
-
-
-			Apply();
-
-
-			heightSmoothLag = oldHeightSmooth;
-		}
-
-
-		/// <summary>
-		/// Sets up the rotation of the camera to always be behind the target
-		/// </summary>
-		/// <param name="centerPos">Center position.</param>
-		void SetUpRotation( Vector3 centerPos )
-		{
-			Vector3 cameraPos = cameraTransform.position;
-			Vector3 offsetToCenter = centerPos - cameraPos;
-
-
-			// Generate base rotation only around y-axis
-			Quaternion yRotation = Quaternion.LookRotation( new Vector3( offsetToCenter.x, 0, offsetToCenter.z ) );
-
-
-			Vector3 relativeOffset = Vector3.forward * distance + Vector3.down * height;
-			cameraTransform.rotation = yRotation * Quaternion.LookRotation( relativeOffset );
-
+			cameraTransform.position = new Vector3 (targetCenter.x, targetCenter.y, -10f);
 
 		}
-
 
 		#endregion
 	}
---
+++ Assets/Scripts/ScollUV.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScollUV : MonoBehaviour
+{
+
+	float parralax = 5f;
+
+	void Update ()
+	{
+		MeshRenderer mr = GetComponent<MeshRenderer> ();
+
+		Material mat = mr.material;
+
+		Vector2 offset = mat.mainTextureOffset;
+		Vector2 position = this.transform.position;
+
+		offset.x = position.x / transform.localScale.x * parralax;
+		offset.y = position.y / transform.localScale.y * parralax;
+
+		mat.mainTextureOffset = this.transform.position;
+	}
+}
